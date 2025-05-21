11544b6510fcf828e05c9f3bd1399e18bd7e5bc3
Juan Carlos Arevalo <jarevaloferrufino@gmail.com>
2025-05-20 21:21:57 -0400

    Add: Network abstraction

--- src/SimpleAccountFactory.sol
+++ src/SimpleAccountFactory.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.23;
+pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/utils/Create2.sol";
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
@@ -15,8 +15,42 @@ import "./SimpleAccount.sol";
 contract SimpleAccountFactory {
     SimpleAccount public immutable accountImplementation;
 
+    mapping(uint256 => address) public routers;
+    mapping(uint256 => address) public links;
+
+    event AccountCreated(
+        uint256 chainid,
+        address router,
+        address link,
+        address account
+    );
+
     constructor(IEntryPoint _entryPoint) {
-        accountImplementation = new SimpleAccount(_entryPoint);
+        //Chainlink testnet routers
+        //Polygon Amoy
+        routers[80002] = 0x9C32fCB86BF0f4a1A8921a9Fe46de3198bb884B2;
+        //Op sepolia
+        routers[11155420] = 0x114A20A10b43D4115e5aeef7345a1A71d2a60C57;
+        //Base Sepolia
+        routers[84532] = 0xD3b06cEbF099CE7DA4AcCf578aaebFDBd6e88a93;
+        //Sepolia
+        routers[11155111] = 0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59;
+
+        //Chainlink testnet links
+        //Polygon Amoy
+        links[80002] = 0x0Fd9e8d3aF1aaee056EB9e802c3A762a667b1904;
+        //Op sepolia
+        links[11155420] = 0xE4aB69C077896252FAFBD49EFD26B5D171A32410;
+        //Base Sepolia
+        links[84532] = 0xE4aB69C077896252FAFBD49EFD26B5D171A32410;
+        //Sepolia
+        links[11155111] = 0x779877A7B0D9E8603169DdbD7836e478b4624789;
+
+        address currentRouter = routers[block.chainid];
+        address currentLink = links[block.chainid];
+        accountImplementation = new SimpleAccount(_entryPoint, currentRouter, currentLink);
+
+        emit AccountCreated(block.chainid, currentRouter, currentLink, address(accountImplementation));
     }
 
     /**
